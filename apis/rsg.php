48ff010bac086b184a188f49366ce981e51e08d3
Bradley Hodges <CheapApples12@users.noreply.github.com>
2017-04-02 16:09:42 +0800

    Update rsg.php

--- apis/rsg.php
+++ apis/rsg.php
@@ -7,35 +7,35 @@
             return $title[1];
         }
     }
-	
+    
     function clear($data) {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data, ENT_QUOTES);
         return $data;
     }
-	
-	$r = clear(urldecode($_REQUEST["url"]));
-	
-	if ($r == "") {
-		http_response_code(406);
+    
+    $r = clear(urldecode($_REQUEST["url"]));
+    
+    if ($r == "") {
+        http_response_code(406);
         header("Content-type: text/json");
-		die('{"error":"request is malformed: url not provided"}');
-	}
+        die('{"error":"request is malformed: url not provided"}');
+    }
     
     if (strpos($r,'socialclub.rockstargames.com/crew') !== false) {
         http_response_code(406);
         header("Content-type: text/json");
-		die('{"error":"request is malformed: url not correct"}');
+        die('{"error":"request is malformed: url not correct"}');
     }
     
     $url = str_replace("Rockstar Games Social Club - Crew : ", "", _title($r));
     if ($url == "" || $url == "Rockstar Games Social Club") {
-		http_response_code(444);
+        http_response_code(444);
         header("Content-type: text/json");
-		die('{"error":"crew does not exist"}');
+        die('{"error":"crew does not exist"}');
     } else {
         header("Content-type: text/json");
-		echo '{"crew_name":"' . $url . '"}';
+        echo '{"crew_name":"' . $url . '"}';
     }
 ?>
